34ee500c0a5d7cf1c4a0e348e8376bfbaf58273b
Surya Vardhan <suryavardhankarella@gmail.com>
2025-05-19 15:07:26 +0530

    UC1 Project Setup and Welcome Endpoint

---
+++ src/main/java/net/suryavardhan/addressbookapp/controller/ContactController.java
@@ -0,0 +1,18 @@
+package net.suryavardhan.addressbookapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/addressbook")
+
+
+public class ContactController {
+	 @GetMapping
+	    public String welcome() {
+	        return "Welcome to Address Book App!";
+
+	 }
+}
+   
