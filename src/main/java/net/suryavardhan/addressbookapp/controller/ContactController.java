da4e802ae079edb1806bf2548ab400ab226e2275
Surya Vardhan <suryavardhankarella@gmail.com>
2025-05-19 15:14:42 +0530

    UC2 Rest Controller with MySQL Application Properties

--- src/main/java/net/suryavardhan/addressbookapp/controller/ContactController.java
+++ src/main/java/net/suryavardhan/addressbookapp/controller/ContactController.java
@@ -1,18 +1,44 @@
 package net.suryavardhan.addressbookapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.suryavardhan.addressbookapp.dto.ContactDTO;
+import net.suryavardhan.addressbookapp.model.Contact;
+import net.suryavardhan.addressbookapp.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/addressbook")
-
-
+@RequestMapping("/contacts")
+@CrossOrigin(origins = "*")
 public class ContactController {
-	 @GetMapping
-	    public String welcome() {
-	        return "Welcome to Address Book App!";
 
-	 }
+    @Autowired
+    private ContactService service;
+
+    @PostMapping
+    public Contact create(@RequestBody ContactDTO dto) {
+        return service.addContact(dto);
+    }
+
+    @GetMapping
+    public List<Contact> getAll() {
+        return service.getAllContacts();
+    }
+
+    @GetMapping("/{id}")
+    public Contact getById(@PathVariable Long id) {
+        return service.getContactById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Contact update(@PathVariable Long id, @RequestBody ContactDTO dto) {
+        return service.updateContact(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        service.deleteContact(id);
+        return "Deleted contact with id: " + id;
+    }
 }
-   
